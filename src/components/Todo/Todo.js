2fb6fc66b48edf2cfac16505cb0e7a0b05564d29
Bryan Loh <bryan_2u@hotmail.com>
2022-04-01 15:36:32 +0800

    Built basic todo app function

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todo = () => {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo) {
+      const todoItem = { id: new Date().getTime().toString(), todo };
+      setTodoList((allList) => {
+        return [...todoList, todoItem];
+      });
+      setTodo("");
+    }
+  };
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
   return (
     <main className="container">
       <h1>Hello World!</h1>
       <form>
         <label htmlFor="todo">Write your todo:</label>
-        <input type="text" name="todo" id="todo" />
-        <button>submit</button>
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          value={todo}
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>submit</button>
+        <div>
+          {todoList.map((item, id) => {
+            const { todo } = item;
+            return <p key={id}>{todo}</p>;
+          })}
+        </div>
       </form>
     </main>
   );
